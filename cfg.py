6c44b899c7c96ee83698303fd01ff2fdd5cc2c2c
sevruk <aleksey.sevruk@gmail.com>
2021-02-04 21:11:38 +0200

    reproduce: config changes

--- cfg.py
+++ cfg.py
@@ -18,10 +18,10 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 Cfg = EasyDict()
 
-Cfg.use_darknet_cfg = True
+Cfg.use_darknet_cfg = False
 Cfg.cfgfile = os.path.join(_BASE_DIR, 'cfg', 'yolov4.cfg')
 
-Cfg.batch = 64
+Cfg.batch = 32
 Cfg.subdivisions = 16
 Cfg.width = 608
 Cfg.height = 608
@@ -45,7 +45,7 @@ Cfg.mosaic = 1
 
 Cfg.letter_box = 0
 Cfg.jitter = 0.2
-Cfg.classes = 80
+Cfg.classes = 184
 Cfg.track = 0
 Cfg.w = Cfg.width
 Cfg.h = Cfg.height
